6a09a124fdb206449d77b23608658c07aae2155b
Allen Mwandunga <allenmcndounger@gmail.com>
2022-11-23 14:23:49 +0300

    test installing in venv other package version from pypi

--- src/app_package_mcndoungerii/model.py
+++ src/app_package_mcndoungerii/model.py
@@ -1,5 +1,23 @@
 
 class General:
 
+    def upper_case(self,text: str):
+        return text.upper()
+
+    def lower_case(self,text: str):
+        return text.lower()
+
+    def title_case(self,text: str):
+        return text.title()
+
+    def capitalize(self,text: str):
+        return text.capitalize()
+
     def is_title(self,text: str):
         return text.istitle()
+
+    def is_upper(self,text: str):
+        return text.isupper()
+
+    def is_lower(self,text: str):
+        return text.islower() 
--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,18 +1,23 @@
 import app_package_mcndoungerii.app as object
 
-# from app_package_mcndoungerii.model import General
+from app_package_mcndoungerii.model import General
 
-# #create a class object
-# object = General()
+#create a class object
+object = General()
 class TestApp:
 
-    print("Object::::::::::::",object)
-
-    # def test_upper(self):
-    #     assert object.upper_case("juma") == "JUMA"
+    def test_upper(self):
+        assert object.upper_case("juma") == "JUMA"
 
     def test_lower(self):
         assert object.lower_case("TOYOTA") == "toyota"
 
-    # def test_is_title(self):
-    #     assert object.is_title("Toyota") == True
+    def test_is_title(self):
+        assert object.is_title("Toyota") == True
+
+    def test_is_lower(self):
+        assert object.is_lower("toyota") == True
+
+    def test_is_upper(self):
+        assert object.is_upper("DOG") == True
+        
